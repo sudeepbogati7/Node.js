0454b6cac61f8fb59f1d8b8f431958919d4df3ac
sudeepbogati7 <sudeepbogati977@gmail.com>
2023-11-26 12:18:19 +0545

    transport logs to database and file using winston

--- vidly-project/index.js
+++ vidly-project/index.js
@@ -18,20 +18,16 @@ const app = express();
 
 
 
-winston.add(new winston.transports.File({ filename : 'logger.log'}));
-winston.add(new winston.transports.MongoDB({
-    db : 'mongodb://127.0.0.1:27017/vidly',
-    level : 'info'
-}));
-
+    
+    
 if(!config.get('myPrivateKey')){
     console.error("FATAL ERROR : The environment variable is not defined ! ");
     process.exit(1);
 }
 
 mongoose.connect('mongodb://127.0.0.1:27017/vidly')
-    .then(() => console.log("Successfully connected to mongoDB ...."))
-    .catch(err => console.log('Something went wrong !', err.message));
+.then(() => console.log("Successfully connected to mongoDB ...."))
+.catch(err => console.log('Something went wrong !', err.message));
 
 
 app.use(express.json());
@@ -45,9 +41,40 @@ app.use('/api/auth', auth);
 app.use(error); //using error middleware at the last , do notice 
 
 
+const logger= winston.createLogger({
+    level : 'info',
+    format : winston.format.simple(),
+    transports : [
+        new winston.transports.File({filename : 'logger.log'}),
+        new winston.transports.MongoDB({db : 'mongodb://127.0.0.1:27017/vidly'})
+    ]
+});
+
+process.on('uncaughtException', ex =>{
+    console.log("Uncaught error occurred ");
+    logger.error(`Uncaught execption : ${ex.message}`);
+    // process.exit(1);
+});
+
+process.on('unhandledRejection', (reason)=>{
+    logger.error(`Unhandled Rejection :  ${reason.message}`);
+});
+
+
+try{
+    throw new Error('This is a simulated eroor ');
+}
+catch(err){
+    logger.error('Uncaught exception  ', err);
+}
+
+
+Promise.reject(new Error("unhaldled promise rejection "));
+
+throw new Error('This is an example of Uncaught exceptions ! '); // this is an example , if we caught an exception after we define and winston error 
+
 
 
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
--- vidly-project/middlewares/error.js
+++ vidly-project/middlewares/error.js
@@ -6,3 +6,5 @@ module.exports = function(err, req, res, next){
 
     res.status(500).send("Internal Server Error !");
 }
+
+
